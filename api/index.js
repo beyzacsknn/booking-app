f092c42db5746313db9405db123bf79243204e13
Beyza Coşkun <32619377+beyzacsknn@users.noreply.github.com>
2023-11-26 16:13:11 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt= require('jsonwebtoken');
 const User= require('./models/User.js');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require ('image-downloader');
 require('dotenv').config();
 const app = express();
 
@@ -13,6 +14,7 @@ const jwtSecret = 'asdfghjkl';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors(
     {
         credentials:true,
@@ -75,6 +77,22 @@ app.get('/profile', (req,res) => {
     } else {
         res.json(null);
     }
+});
+
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json(true);
+});
+
+console.log({__dirname});
+app.post('/upload-by-link', async (req,res) => 
+{
+    const {link} =req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/uploads/' + newName,
+    });
+    res.json(newName);
 })
 
 app.listen(4000);
